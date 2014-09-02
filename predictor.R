f3b2a38c4d749c486589ef4eef6a02e3a7c04122
Igor Subbotin <moat1981@gmail.com>
2014-09-03 01:14:48 +0400

    0.49424 (225)

--- predictor.R
+++ predictor.R
@@ -16,40 +16,72 @@ prepare <- function(d) {
 }
 
 library(caret)
+library(plyr)
 
 # training
-#training <- train[,-which(names(train) %in% c("casual","registered"))]
 training <- loadTrain()
 training <- prepare(training)
-modFitCasual <- train(casual~hour+workingday+humidity+temp+weather,data=training,method="rf",
-                trControl=trainControl(method="repeatedcv",number=10,repeats=3,allowParallel=TRUE,verboseIter=TRUE))
-varImp(modFitCasual)
-modFitRegistered <- train(registered~hour+workingday+humidity+temp+weather,data=training,method="rf",
-                          trControl=trainControl(method="repeatedcv",number=10,repeats=3,allowParallel=TRUE,verboseIter=TRUE))
-#varImp(modFitRegistered)
-training$casualPred <- predict(modFitCasual,newdata=training)
-training[training$casualPred<0,]$casualPred <- 0
-training$registeredPred <- predict(modFitRegistered,newdata=training)
-training[training$registeredPred<0,]$registeredPred <- 0
-training$countPred <- training$casualPred + training$registeredPred
-training[training$countPred<0,]$countPred <- 0
 
-rmseCasual <- rmse(training$casualPred,training$casual)
-rmseRegistered <- rmse(training$registeredPred,training$registered)
-rmseCount <- rmse(training$countPred,training$count)
-rmsleCasual <- rmsle(training$casualPred,training$casual)
-rmsleRegistered <- rmsle(training$registeredPred,training$registered)
-rmsleCount <- rmsle(training$countPred,training$count)
+# working day
+trainingWday <- subset(training, workingday==1)
+modFitCasualWday <- train(casual~hour+humidity+temp+weather+season,data=trainingWday,method="rf",
+                trControl=trainControl(method="repeatedcv",number=10,repeats=3,allowParallel=TRUE,verboseIter=TRUE))
+modFitRegisteredWday <- train(registered~hour+humidity+temp+weather+season,data=trainingWday,method="rf",
+                          trControl=trainControl(method="repeatedcv",number=10,repeats=3,allowParallel=TRUE,verboseIter=TRUE))
+trainingWday$casualPred <- predict(modFitCasualWday,newdata=trainingWday)
+trainingWday$registeredPred <- predict(modFitRegisteredWday,newdata=trainingWday)
+trainingWday$countPred <- trainingWday$casualPred + trainingWday$registeredPred
+rmseCasualWday <- rmse(trainingWday$casualPred,trainingWday$casual)
+rmseRegisteredWday <- rmse(trainingWday$registeredPred,trainingWday$registered)
+rmseCountWday <- rmse(trainingWday$countPred,trainingWday$count)
+rmsleCasualWday <- rmsle(trainingWday$casualPred,trainingWday$casual)
+rmsleRegisteredWday <- rmsle(trainingWday$registeredPred,trainingWday$registered)
+rmsleCountWday <- rmsle(trainingWday$countPred,trainingWday$count)
+
+# holiday
+trainingHday <- subset(training, workingday==0)
+modFitCasualHday <- train(casual~hour+humidity+temp+weather+season,data=trainingHday,method="rf",
+                      trControl=trainControl(method="repeatedcv",number=10,repeats=3,allowParallel=TRUE,verboseIter=TRUE))
+modFitRegisteredHday <- train(registered~hour+humidity+temp+weather+season,data=trainingHday,method="rf",
+                          trControl=trainControl(method="repeatedcv",number=10,repeats=3,allowParallel=TRUE,verboseIter=TRUE))
+trainingHday$casualPred <- predict(modFitCasualHday,newdata=trainingHday)
+trainingHday$registeredPred <- predict(modFitRegisteredHday,newdata=trainingHday)
+trainingHday$countPred <- trainingHday$casualPred + trainingHday$registeredPred
+rmseCasualHday <- rmse(trainingHday$casualPred,trainingHday$casual)
+rmseRegisteredHday <- rmse(trainingHday$registeredPred,trainingHday$registered)
+rmseCountHday <- rmse(trainingHday$countPred,trainingHday$count)
+rmsleCasualHday <- rmsle(trainingHday$casualPred,trainingHday$casual)
+rmsleRegisteredHday <- rmsle(trainingHday$registeredPred,trainingHday$registered)
+rmsleCountHday <- rmsle(trainingHday$countPred,trainingHday$count)
+
+# score on full training set
+trainingCheck <- rbind(trainingWday, trainingHday)
+trainingCheck <- arrange(trainingCheck, datetime)
+rmseCasual <- rmse(trainingCheck$casualPred,trainingCheck$casual)
+rmseRegistered <- rmse(trainingCheck$registeredPred,trainingCheck$registered)
+rmseCount <- rmse(trainingCheck$countPred,trainingCheck$count)
+rmsleCasual <- rmsle(trainingCheck$casualPred,trainingCheck$casual)
+rmsleRegistered <- rmsle(trainingCheck$registeredPred,trainingCheck$registered)
+rmsleCount <- rmsle(trainingCheck$countPred,trainingCheck$count)
 
 # testing
 test <- loadTest()
-#test$datetime <- as.character(test$datetime)
-#test$datetime <- as.POSIXlt(test$datetime, "%Y-%m-%d %H:%M:%S", tz = "GMT")
 testing <- prepare(test)
-test$casual <- predict(modFitCasual,newdata=testing)
-test[test$casual<0,]$casual <- 0
-test$registered <- predict(modFitRegistered,newdata=testing)
-test[test$registered<0,]$registered <- 0
-test$count <- test$casual + test$registered
-test[test$count<0,]$count <- 0
-write.csv(test[,c("datetime", "count")],file="submission.csv",row.names=F,col.names=T,quote=FALSE)
+
+# working day
+testingWday <- subset(testing, workingday==1)
+testingWday$casual <- predict(modFitCasualWday,newdata=testingWday)
+testingWday$registered <- predict(modFitRegisteredWday,newdata=testingWday)
+testingWday$count <- testingWday$casual + testingWday$registered
+
+# holiday
+testingHday <- subset(testing, workingday==0)
+testingHday$casual <- predict(modFitCasualHday,newdata=testingHday)
+testingHday$registered <- predict(modFitRegisteredHday,newdata=testingHday)
+testingHday$count <- testingHday$casual + testingHday$registered
+
+# combine results
+testingResult <- rbind(testingWday, testingHday)
+testingResult <- arrange(testingResult, datetime)
+testingResult[testingResult$count<0,]$count <- 0
+write.csv(testingResult[,c("datetime", "count")],file="submission.csv",row.names=F,col.names=T,quote=FALSE)
